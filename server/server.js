6cfc195b8bf2759b449820b30db3e028dca4bf32
effcamp <fcf.camposx@gmail.com>
2017-12-11 15:37:33 -0200

    Added sockets

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', socket => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
