407f569bb21edec47ade39cfd1c5b40a7041ded4
effcamp <fcf.camposx@gmail.com>
2017-12-11 14:56:31 -0200

    Initial commit of server websocket Chat app

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
+});
