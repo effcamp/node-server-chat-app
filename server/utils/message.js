fb0a95c307493d520e21d1c03bb0dc61b2ebbfb2
effcamp <fcf.camposx@gmail.com>
2017-12-12 11:28:41 -0200

    Added moment format time

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,14 @@
+const moment = require('moment');
+
 const generateMessage = (from, text) => ({
   from,
   text,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 
 const generateLocationMessage = (from, lat, lng) => ({
   from,
   url: `https://google.com/maps?q=${lat},${lng}`,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 module.exports = { generateMessage, generateLocationMessage };
