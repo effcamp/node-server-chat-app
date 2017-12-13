1fbf5660b996960b462b7ae5ef9f40f6eb579d93
effcamp <fcf.camposx@gmail.com>
2017-12-13 13:23:27 -0200

    Added join page (scrolling and layout not working

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,24 @@
 const socket = io();
 
+function scrollToBottom() {
+  //Selectors
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child');
+  //Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -12,8 +31,8 @@ socket.on('newMessage', function(message) {
     from: message.from,
     createdAt: formattedTime
   });
-
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -26,6 +45,7 @@ socket.on('newLocationMessage', function(message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('disconnect', function() {
